304b631ed443602299e0c105f186aff4a854adcd
Rushikesh Yemul <yemulrushikesh56@gmail.com>
2025-01-10 23:09:20 +0530

    shifted mongoDB to onlice Server

--- db.js
+++ db.js
@@ -1,31 +1,24 @@
-const mongoose = require("mongoose"); // IT IS JUST LIKE IMPORT STATEMENT
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-// MongoDB URL
+// const mongoURL = process.env.MONGODB_URL_LOCAL; // IT IS LOCAL URL
+const mongoURL = process.env.MONGODB_URL; // IT IS Online URL (Atlas)
 
-const mongoURL = "mongodb://localhost:27017/hotels"; // "after the / slash you can add your db Name"
-
-// setup MongoDB Connection
-
-mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true });
-
-// Mongoose maintains a default connection object representing the MongoDB connection
+mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true }); // setup MongoDB Connection
 
 const db = mongoose.connection; // This object is used for interaction
 
 // Defining the event listeners for database connections
 
 db.on("connected", () => {
-  //   alert("Connected To MongoDB Server");
   console.log("Connected To MongoDB Server");
 });
 
 db.on("error", (err) => {
-  //   alert("Connected To MongoDB Server");
   console.log("Connection Error:", err);
 });
 
 db.on("disconnected", () => {
-  //   alert("Connected To MongoDB Server");
   console.log("MongoDB Disconnected");
 });
 
--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const db = require("./db"); // Import the database connection
 const app = express(); // Initialize the Express application
+require("dotenv").config();
 
 app.use(bodyParser.json()); // Middleware to parse JSON request bodies
+
+const PORT = process.env.PORT || 3000;
 
 // Default route for the application
 app.get("/", (req, res) => {
@@ -19,6 +22,7 @@ app.use("/person", personRoutes); // Use the Routers
 app.use("/menuItem", menuRoutes);
 
 // Start the server on port 3000
-app.listen(3000, () => {
+
+app.listen(PORT, () => {
   console.log("Server is running at http://localhost:3000");
 });
